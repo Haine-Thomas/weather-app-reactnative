cc0b303c9b74cbfd5e7a3f8fb321d4bba8bd42a2
Mushi2216 <thomahaine@gmail.com>
2020-10-08 17:29:13 +0200

    app finished, components ok, adding a module to change units ok, adding a reload icon to refresh ok

--- App.js
+++ App.js
@@ -1,42 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location';
+import WeatherInfo from './components/WeatherInfo';
+import UnitsPicker from './components/UnitsPicker';
+import ReloadIcon from './components/ReloadIcon';
+import WeatherDetails from './components/WeatherDetails';
+import { colors } from './utils/index';
+import { WEATHER_API_KEY } from 'react-native-dotenv'
 
-const WEATHER_API_KEY = '99fe1bd524c3185e---34af554bc5fd9532';
+
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 export default function App() {
-  const {errorMessage, setErrorMessage} = useState(null);
-  useEffect(()=> {
-    load();
-  }, []);
-  async function load() {
-    try {
-      let { status } = await Location.requestPermissionsAsync();
-      if(status !== 'granted') {
-        setErrorMessage('Access to location is needed to run the app')
-        return
-      }
-      const location = await Location.getCurrentPositionAsync();
-      const {latitude, longitude} = location.coords;
-      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
-    } catch (error) {
-      
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [currentWeather, setCurrentWeather] = useState(null);
+    const [unitsSystem, setUnitsSystem] = useState('metric');
+
+    useEffect(() => {
+        load()
+    }, [unitsSystem])
+
+    async function load() {
+        setCurrentWeather(null)
+        setErrorMessage(null)
+        try {
+            let { status } = await Location.requestPermissionsAsync()
+
+            if (status !== 'granted') {
+                setErrorMessage('Access to location is needed to run the app')
+                return
+            }
+            const location = await Location.getCurrentPositionAsync()
+
+            const { latitude, longitude } = location.coords
+
+            const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+
+            const response = await fetch(weatherUrl)
+
+            const result = await response.json()
+
+            if (response.ok) {
+                setCurrentWeather(result)
+            } else {
+                setErrorMessage(result.message)
+            }
+        } catch (error) {
+            setErrorMessage(error.message)
+        }
     }
-  }
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    if (currentWeather) {
+        return (
+            <View style={styles.container}>
+                <StatusBar style="auto" />
+                <View style={styles.main}>
+                    <UnitsPicker unitsSystem={unitsSystem} setUnitsSystem={setUnitsSystem} />
+                    <ReloadIcon load={load} />
+                    <WeatherInfo currentWeather={currentWeather} />
+                </View>
+                <WeatherDetails currentWeather={currentWeather} unitsSystem={unitsSystem} />
+            </View>
+        )
+    } else if (errorMessage) {
+        return (
+            <View style={styles.container}>
+                <ReloadIcon load={load} />
+                <Text style={{ textAlign: 'center' }}>{errorMessage}</Text>
+                <StatusBar style="auto" />
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color={colors.PRIMARY_COLOR} />
+                <StatusBar style="auto" />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    main: {
+        justifyContent: 'center',
+        flex: 1,
+    },
+})
